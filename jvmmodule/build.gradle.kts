58ed66b4118a802da75c3e3693346ee12eee80dd
jamiecraane <jcraane@gmail.com>
2020-11-05 17:00:43 +0100

    Remove unused dependencies

--- jvmmodule/build.gradle.kts
+++ jvmmodule/build.gradle.kts
@@ -12,9 +12,6 @@ application {
 
 dependencies {
     implementation(project(":shared"))
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
-    /*implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.0-rc")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")*/
     implementation(kotlin("stdlib-jdk8"))
 }
 repositories {
