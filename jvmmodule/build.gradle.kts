a4e97baab9cb0c8e41682256ac5b87d3a1b424f3
jamiecraane <jcraane@gmail.com>
2020-11-06 09:08:39 +0100

    jvmmodule as kmp module

--- jvmmodule/build.gradle.kts
+++ jvmmodule/build.gradle.kts
@@ -1,27 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    application
-    kotlin("jvm")
-    kotlin("kapt")
+//    application
+    kotlin("multiplatform")
 }
 
+repositories {
+    gradlePluginPortal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+
+
+/*
 application {
     mainClassName = "TestKt"
 }
+*/
 
-dependencies {
-    implementation(project(":shared"))
-    implementation(kotlin("stdlib-jdk8"))
-}
-repositories {
-    mavenCentral()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+kotlin {
+    jvm()
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":shared"))
+            }
+        }
+    }
 }
